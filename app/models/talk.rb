4fcf55254ab76dcb8403b62cd3cfe04a7e8bb89a
Rohan Mitchell <rohan@ru.homelinux.net>
2009-11-18 14:26:19 +1100

    Fetch info for top talks page

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,6 +1,14 @@
 class Talk < ActiveRecord::Base
   belongs_to :presenter, :class_name => 'User', :foreign_key => 'user_id'
+  has_many :votes
 
   validates_presence_of :title, :presenter, :description
   validates_length_of :description, :maximum => 140
+
+
+  def self.all_ordered_by_votes
+    talks = Talk.all
+    talks = talks.sort_by { |talk| talk.votes.count } # TODO: secondary sort by created_at DESC
+    talks
+  end
 end
--- test/functional/talks_controller_test.rb
+++ test/functional/talks_controller_test.rb
@@ -1,8 +1,13 @@
 require 'test_helper'
 
 class TalksControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "should get top talks page" do
+    get :index
+    assert_response :success
+
+    talks = Talk.all
+    talks = talks.sort_by { |talk| talk.votes.count } # TODO: secondary sort by created_at DESC
+
+    assert_equal talks, assigns(:talks)
   end
 end
