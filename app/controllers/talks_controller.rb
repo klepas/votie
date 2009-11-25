1c1f47f1e7b33acb716f2f551fb6c436dc477212
Rohan Mitchell <rohan@ru.homelinux.net>
2009-11-25 15:34:22 +1100

    Submit talks, split talk display into a partial, fixed stylesheet link, added links on home page, added some rudimentary css for talk display

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -17,4 +17,21 @@ class TalksController < ApplicationController
   def presenters
     @presenters = User.all_presenters
   end
+
+
+  def new
+    @talk = Talk.new
+  end
+
+  def create
+    @talk = Talk.new(params[:talk])
+    @talk.presenter = @user
+
+    if @talk.save
+      flash[:notice] = 'Your exceedingly awesome talk was added to the list. Good luck!'
+      redirect_to talks_path
+    else
+      render :action => "new"
+    end
+  end
 end
--- test/functional/talks_controller_test.rb
+++ test/functional/talks_controller_test.rb
@@ -27,4 +27,48 @@ class TalksControllerTest < ActionController::TestCase
 
     assert_equal [1, 2], assigns(:presenters).map { |p| p.id }
   end
+
+
+  test "should get new talk page" do
+    get :new, {}, {:user_id => users(:one).id}
+    assert_response :success
+  end
+
+
+  test "create should fail on missing fields" do
+    # Title
+    assert_difference('Talk.count', 0) do
+      post :create, {:talk => {:title => '', :description => 'description', :link => 'linky'}}, {:user_id => users(:one).id}
+    end
+    assert_response :success
+
+    # Description
+    assert_difference('Talk.count', 0) do
+      post :create, {:talk => {:title => 'Title', :description => '', :link => 'linky'}}, {:user_id => users(:one).id}
+    end
+    assert_response :success
+  end
+
+
+  test "should create talk" do
+    title = 'title'
+    description = 'description'
+    link = 'link'
+
+    assert_difference('Talk.count') do
+      post(:create,
+           {:talk => {:title => title,
+                      :description => description,
+                      :link => link}},
+           {:user_id => users(:one).id})
+    end
+
+    assert_redirected_to talks_url
+
+    talk = assigns(:talk)
+    assert_equal users(:one), talk.presenter
+    assert_equal title, talk.title
+    assert_equal description, talk.description
+    assert_equal link, talk.link
+  end
 end
