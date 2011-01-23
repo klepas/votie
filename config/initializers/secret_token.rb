01d5f590a68a73e3b1873ebe1c418fabc797204b
Rohan Mitchell <rohan@rohanmitchell.com>
2011-01-24 10:20:21 +1100

    Started upgrade to Rails 3 with 'rails new .'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Votie::Application.config.secret_token = 'a25251d39f1f07fa077ed8d1ffa819cb68eae9ae523f2fde7fa539c72f74cd101c365e9612c39ae0c563649e2ae3ce9722dd917146c04587fb62b5e4b4e923d2'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_start_session',
-  :secret      => 'dfcb585dc98c119bdc5e75f570907e9639009c023384c45f99bd11e634c0b3dffa6363c694334a364a4fbbe7390b05254eb2f630598b35f8e6c80b1d891915e9'
-}
+Votie::Application.config.session_store :cookie_store, :key => '_votie_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# Votie::Application.config.session_store :active_record_store
