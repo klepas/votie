28b9b0b848da76605bbd8c2390f37d47e22c33f1
Rohan Mitchell <rohan@rohanmitchell.com>
2011-01-24 11:30:21 +1100

    Updated deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,77 +1,101 @@
-set :webfaction_username, "niftykit"
-set :webfaction_db_type, "mysql"
-set :webfaction_db, "niftykit_votie"
-set :webfaction_db_username, "niftykit_votie"
-set :webfaction_port, "51977"
-set :database_yml_template, "database.yml"
+default_run_options[:pty] = true  # Must be set for the password prompt from git to work
+ssh_options[:forward_agent] = true
 
-set :ruby, "/home/niftykit/ruby1.8/bin/ruby"
-set :rake, "/home/niftykit/ruby1.8/lib/ruby/gems/1.8/bin/rake"
- 
+set :rake,    '~/ruby1.8/lib/ruby/gems/1.8/bin/rake'
+set :bundle,   '/home/niftykit/ruby1.8/lib/ruby/gems/1.8/bin/bundle'
+
 set :application, "votie"
-set :deploy_to, "/home/#{webfaction_username}/webapps-releases/#{application}"
- 
-set :scm, :git
-set :scm_user, "RohanM"
-set :repository, "git@github.com:klepas/votie.git"
-set :branch, "master"
-set :deploy_via, :remote_cache
-#default_run_options[:pty] = true
+set :repository, "git@github.com:klepas/#{application}.git"
+set :deploy_to,   "/home/niftykit/webapps/votie_2011/#{application}"
+set :use_sudo,    false
+set :user,        'niftykit'
 
-set :user, "#{webfaction_username}"
-set :use_sudo, false 
- 
-set :domain, "votie.niftykit.com"
+set :scm,         :git
+set :scm_command, "~/bin/git"
+set :local_scm_command, "/usr/bin/git"
 
-role :app, domain
-role :web, domain
-role :db,  domain, :primary => true
- 
-desc "Symlink public to what webfaction expects the webroot to be"
-task :after_symlink, :roles => :web do
-  run "ln -nfs #{release_path}/public /home/#{webfaction_username}/webapps/#{application}/"
-end
- 
+default_environment["PATH"] = "$PATH:/home/niftykit/bin"
+default_environment["GEM_HOME"] = "#{deploy_to}/../gems"
+default_environment["GEM_PATH"] = "#{deploy_to}/../gems"
+
+role :web, "votieapp.org"                           # Your HTTP server, nginx/Apache/etc
+role :app, "votieapp.org"                           # This may be the same as your `Web` server
+role :db,  "votieapp.org", :primary => true         # This is where Rails migrations will run
+
 namespace :deploy do
- 
-  # Taken from http://jonathan.tron.name/2006/07/15/capistrano-password-prompt-tips 
-  # Thanks Jonathan! :)
-#  desc "Creates the database configuration on the fly"
-#  task :create_database_configuration, :roles => :app do
-#    require "yaml"
-#    set :production_db_password, proc { Capistrano::CLI.password_prompt("Remote production database password: ") }
-# 
-#    db_config = YAML::load_file("config/#{database_yml_template}")
-#    db_config.delete('test')
-#    db_config.delete('development')
-# 
-#    db_config['production']['adapter'] = "#{webfaction_db_type}"
-#    db_config['production']['database'] = "#{webfaction_db}"
-#    db_config['production']['username'] = "#{webfaction_db_username}"
-#    db_config['production']['password'] = production_db_password
-#    db_config['production']['host'] = "localhost"
-# 
-#    put YAML::dump(db_config), "#{release_path}/config/database.yml", :mode => 0664
-#  end
-# 
-#  after "deploy:update_code", "deploy:create_database_configuration"
- 
-  desc "Redefine deploy:start"
   task :start, :roles => :app do
-    invoke_command "$HOME/ruby1.8/lib/ruby/gems/1.8/bin/mongrel_rails start -c #{deploy_to}/current -d -e production -P /home/#{webfaction_username}/webapps/#{application}/log/mongrel.pid -p #{webfaction_port}", :via => run_method
+    run "touch #{current_release}/tmp/restart.txt"
   end
- 
-  desc "Redefine deploy:restart"
-  task :restart, :roles => :app do
-    # This command fails to restart mongrel
-    #invoke_command "$HOME/ruby1.8/lib/ruby/gems/1.8/bin/mongrel_rails restart -c #{deploy_to}/current -P /home/#{webfaction_username}/webapps/#{application}/log/mongrel.pid", :via => run_method
 
-    invoke_command "$HOME/ruby1.8/lib/ruby/gems/1.8/bin/mongrel_rails stop -c #{deploy_to}/current -P /home/#{webfaction_username}/webapps/#{application}/log/mongrel.pid", :via => run_method
-    invoke_command "$HOME/ruby1.8/lib/ruby/gems/1.8/bin/mongrel_rails start -c #{deploy_to}/current -d -e production -P /home/#{webfaction_username}/webapps/#{application}/log/mongrel.pid -p #{webfaction_port}", :via => run_method
-  end
- 
-  desc "Redefine deploy:stop"
   task :stop, :roles => :app do
-    invoke_command "$HOME/ruby1.8/lib/ruby/gems/1.8/bin/mongrel_rails stop -c #{deploy_to}/current -P /home/#{webfaction_username}/webapps/#{application}/log/mongrel.pid", :via => run_method
+    # Do nothing.
+  end
+  
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+  
+  desc "run 'rake db:seed' in production"
+  task :load_seed do
+    run "cd #{current_release} &&  #{rake} db:seed"
   end
 end
+
+namespace :migrations do
+  task :redo, :roles => :app do
+    run("cd #{current_release} && #{rake} db:migrate VERSION=00 RAILS_ENV=production && #{rake} db:migrate RAILS_ENV=production")
+  end
+end
+
+namespace :db do
+  task :seed, :roles => :app do
+    run("cd #{current_release} && #{rake} db:seed RAILS_ENV=production")
+  end
+  task :reset, :roles => :app do
+    run("cd #{current_release} && #{rake} db:reset RAILS_ENV=production")
+  end
+  task :migrate, :roles => :app do
+    run("cd #{current_release} && #{rake} db:migrate RAILS_ENV=production")
+  end
+end
+
+namespace :bundler do
+  task :install, :roles => :app do
+    run "cd #{current_release} && #{bundle} install --without development"
+  end
+
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(current_release, '.bundle')
+    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+  end
+  
+  task :bundle_new_release, :roles => :app do
+    bundler.create_symlink
+    run "cd #{release_path} && #{bundle} install --without development"
+  end
+  
+  task :lock, :roles => :app do
+    run "cd #{current_release} && #{bundle} lock;"
+  end
+  
+  task :unlock, :roles => :app do
+    run "cd #{current_release} && #{bundle} unlock;"
+  end
+end
+
+after "deploy:update_code" do
+  bundler.bundle_new_release
+  deploy.symlink_shared
+  # deploy.migrations
+  # deploy.load_seed
+  # ...
+end
+# after 'deploy:update_code', 'deploy:symlink_shared' #, 'deploy:migrations', 'deploy:load_seed'
