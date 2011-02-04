b93dd5b0e33ee851d3f0db5ee062003ed5454ac5
Rohan Mitchell <rohan.mitchell@anu.edu.au>
2011-02-04 14:03:30 +1100

    Collect name and twitter name on registration form. Mirror username to twitter name with JS.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,25 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(document).ready(function() {
+	$("form#new_user #user_login").mirrorFieldTo("form#new_user #user_twitter_name");
+    });
+
+
+/**
+ * Mirror one field to another, as long as the target field has not been modified
+ * Usage: $("#my_field").mirrorFieldTo("#another_field");
+ */
+jQuery.fn.mirrorFieldTo = function(target_selector) {
+    var target = jQuery(target_selector);
+
+    // Keep track of whether the target field has been modified
+    target.data('modified', false);
+    target.change(function() { jQuery(this).data('modified', true); });
+
+
+    // If the target hasn't been modified, mirror the source field to it
+    var mirror_fn = function() {
+	if(!target.data('modified')) {
+	    target.val(jQuery(this).val());
+	}
+    };
+    jQuery(this).keyup(mirror_fn).blur(mirror_fn);
+}
