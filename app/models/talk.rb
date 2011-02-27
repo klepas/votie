2a9d5c7796a3cddc1aeda51780ae0b33c1e09ac5
Rohan Mitchell <rohan@rohanmitchell.com>
2011-02-27 11:17:22 +1100

    First cuc test passes

--- app/models/talk.rb
+++ app/models/talk.rb
@@ -4,15 +4,16 @@ class Talk < ActiveRecord::Base
   belongs_to :presenter, :class_name => 'User', :foreign_key => 'user_id'
   has_many :votes, :dependent => :destroy
 
-  attr_accessor :creator_is_presenter
-
+  accepts_nested_attributes_for :presenter
+  
   validates_presence_of :title, :creator, :presenter, :description
   validates_length_of :description, :maximum => 140
 
   scope :ordered_by_votes, joins('LEFT OUTER JOIN votes ON (votes.talk_id=talks.id)').group('talks.id').order('COUNT(votes.id) DESC, talks.id DESC')
 
   after_initialize :set_default_values
-  before_save :ensure_link_is_absolute, :set_presenter
+  before_validation :set_presenter
+  before_save :ensure_link_is_absolute
 
   def set_default_values
     self.link ||= "http://"
@@ -29,7 +30,19 @@ class Talk < ActiveRecord::Base
   end
 
   def set_presenter
+    # Set the presenter
     self.presenter = self.creator if self.creator_is_presenter
+
+    # If we're creating a new presenter, initialise the user name and password
+    if self.presenter.new_record?
+      self.presenter.login = self.presenter.twitter_name
+      self.presenter.login = self.presenter.name.gsub(/ /, '-') if self.presenter.login.blank?
+
+      password = Authlogic::Random.friendly_token
+      self.presenter.password = password
+      self.presenter.password_confirmation = password
+    end
+    
   end
 
   def num_votes
@@ -48,5 +61,13 @@ class Talk < ActiveRecord::Base
     else
       'other'
     end
+  end
+
+
+  def creator_is_presenter
+    @creator_is_presenter
+  end
+  def creator_is_presenter=(val)
+    @creator_is_presenter = (val == '1')
   end
 end
--- features/talk-by-other.feature
+++ features/talk-by-other.feature
@@ -19,13 +19,14 @@ Feature: Submitting talks not presented by yourself
     And I fill in "talk_description" with "This talk is awesome!"
     And I fill in "talk_link" with "http://google.com"
     And I choose "Someone else"
-    And I fill in "talk_presenter_name" with "Pascal Klein"
-    And I fill in "talk_presenter_twitter_name" with "klepas"
+    And I fill in "talk_presenter_attributes_name" with "Pascal Klein"
+    And I fill in "talk_presenter_attributes_twitter_name" with "klepas"
     And I press "submit"
-    And I go to the talks page
-    Then I should see "My Spiffy Talk" within ".talk-title"
-    And I should see "Pascal Klein" within ".talk-presenter"
-    And there should be a user with login: "klepas"
+    Then I should see "Your exceedingly awesome talk was added to the list. Good luck!"
+    When I go to the talks page
+    Then I should see "My Spiffy Talk" within ".talk .title"
+    And I should see "Pascal Klein" within ".talk .presenter"
+    And a user should exist with login: "klepas", name: "Pascal Klein"
 
 
   Scenario: editing a talk created and presented by the same person
